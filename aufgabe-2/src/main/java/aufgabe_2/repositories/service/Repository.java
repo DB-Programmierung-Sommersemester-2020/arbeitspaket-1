af20b89fe95b1fe2960cf8ab81e29bb8fbcb5f44
VChouliak <chouliak@hotmail.com>
2020-05-03 22:54:14 +0200

    Initialize Repository - Service

---
+++ aufgabe-2/src/main/java/aufgabe_2/repositories/service/Repository.java
@@ -0,0 +1,12 @@
+package aufgabe_2.repositories.implementations;
+
+import java.util.List;
+
+public interface Repository <T, K>{
+    
+    List<T> getAll();
+    T getById(K id);
+    T create(T entity);
+    T update(T entity);
+    T delete(T entity);
+}
