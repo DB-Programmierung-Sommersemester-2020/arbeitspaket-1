fabcc16a4dc7c8f1c0e46ae6ffef544d35c21d9b
VChouliak <chouliak@hotmail.com>
2020-05-04 20:43:24 +0200

    Initial commit for aufgabe_5

---
+++ aufgabe_5/jdbc_client_b/src/main/java/jdbc_client_b/App.java
@@ -0,0 +1,13 @@
+package jdbc_client_b;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
